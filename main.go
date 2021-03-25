5c2c967b0025cbdd833469b634f42472514be19f
jhaoheng <jhaoheng@gmail.com>
2021-03-25 23:02:29 +0800

    update

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"google-oauth2-bot/googleoauth2"
 	"net/http"
+	"os"
 	"strings"
 )
 
@@ -12,16 +13,30 @@ var client_id, cliet_secret, redirect_uri string
 var port = 8080
 
 func main() {
-	flag.StringVar(&client_id, "id", "", "")
-	flag.StringVar(&cliet_secret, "secret", "", "")
-	flag.StringVar(&redirect_uri, "redirectUri", "", "")
+	flag.Usage = func() {
+		fmt.Fprintln(os.Stderr, "[usage]")
+		fmt.Fprintln(os.Stderr, "-id=<value>          : Google oauth2 client_id")
+		fmt.Fprintln(os.Stderr, "-secret=<value>      : Google oauth2 cliet_secret")
+		fmt.Fprintln(os.Stderr, "-redirectUri=<value> : Google oauth2 redirect_uri")
+
+		fmt.Fprintln(os.Stderr, "\nRef this URL to get correlation variables, https://developers.google.com/identity/protocols/oauth2")
+	}
+	flag.StringVar(&client_id, "id", "", "client_id")
+	flag.StringVar(&cliet_secret, "secret", "", "cliet_secret")
+	flag.StringVar(&redirect_uri, "redirectUri", "", "redirect_uri")
 	flag.Parse()
+
+	if len(client_id) == 0 || len(cliet_secret) == 0 || len(redirect_uri) == 0 {
+		flag.Usage()
+		return
+	}
+
 	fmt.Println("======")
 	fmt.Printf("client_id    => %s\n", client_id)
 	fmt.Printf("cliet_secret => %s\n", cliet_secret)
 	fmt.Printf("redirect_uri => %s\n", redirect_uri)
 	fmt.Println("======")
-
+	//
 	o := googleoauth2.New(client_id, cliet_secret, redirect_uri)
 	o.ApplyCode()
 
